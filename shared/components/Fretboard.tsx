88f777b72027d57d03e72686664d9730548749fb
Mike Cullimore <michael.cullimore11@gmail.com>
2024-02-26 23:51:03 +0000

    Set up Storybook.

---
+++ shared/components/Fretboard.tsx
@@ -0,0 +1,5 @@
+// TODO: copy from guitar-navigation repo.
+
+export const Fretboard = ({ numFrets, numStrings }: { numFrets: number, numStrings: number }) => {
+    return `Fretboard with ${numStrings} strings, ${numFrets} frets.`
+}
